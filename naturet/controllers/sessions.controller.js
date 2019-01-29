2c85de83e76116bd032e8a27337e0f6129f6dabd
Moises Garcia <moisesgarcia83@gmail.com>
2019-01-29 11:36:24 +0100

    https://github.com/naturet/proyect/issues/1

---
+++ naturet/controllers/sessions.controller.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+
+module.exports.create = (req, res, next) => {
+  res.render('sessions/create')
+}
+
+
+module.exports.createWithIDPCallback = (req, res, next) => {
+  passport.authenticate(`${req.params.provider}-auth`, (error, user) => {
+    if (error) {
+      next(error);
+    } else {
+      req.login(user, (error) => {
+        if (error) {
+          next(error)
+        } else {
+          res.redirect('/users')
+        }
+      })
+    }
+  })(req, res, next);
+}
---
+++ naturet/routes/sessions.routes.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+const express = require('express');
+const router = express.Router();
+const sessionsController = require('../controllers/sessions.controller');
+
+router.get('/create', sessionsController.create);
+// router.post('/create', sessionsController.doCreate);
+
+router.post('/google', passport.authenticate('google-auth', { scope: ['openid', 'profile', 'email'] }));
+
+router.get('/:provider/cb', sessionsController.createWithIDPCallback);
+
+
+module.exports = router;
