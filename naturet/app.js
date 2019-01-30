def96b2c61927640eaf022c70c628bcfb19ed34d
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-01-30 20:30:20 +0100

    test change github

--- naturet/app.js
+++ naturet/app.js
@@ -39,7 +39,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler in appjs
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
