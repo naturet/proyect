6ce48c33b32a6eec9004e162e96ecfdc5970ec85
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-02-16 21:51:34 +0100

    feat: notify at buying experience

--- public/js/send.js
+++ public/js/send.js
@@ -1,12 +1,17 @@
 $(document).ready(() => {
-
+  
   const sendPol = $("#sendpolitics")
   function sendPolitic(){  
+    $("#sendpolitics").notify(
+      "We have sent your invoice", "success",
+      { elementPosition: 'topleft'}
+    );
        axios.post(`/experiences/send`, { method: "post", withCredentials: true })
          .then(response => {
+         
          })
          .catch(error => console.error(error));
    };
   
   sendPol.click(sendPolitic)
-  })    
+  })  
--- public/js/subscribe.js
+++ public/js/subscribe.js
@@ -1,12 +1,49 @@
 $(document).ready(() => {
 
-  const sendMail = $("#sendSub"); 
-  function subscribe(){      
-        var to=$("#subscribe").val()     
-        axios.post(`/subscribe`, {to:to, method: "post", withCredentials: true })
-          .then(response => {
-          })
-          .catch(error => console.error(error));
-    }
+  const sendMail = $("#sendSub");
+
+  function subscribe() {
+    var to = $("#subscribe").val()
+    axios.post(`/subscribe`, {
+        to: to,
+        method: "post",
+        withCredentials: true
+      })
+      .then(response => {})
+      .catch(error => console.error(error));
+  }
   sendMail.click(subscribe)
-  })
+})
+
+$(document).ready(() => {
+
+  const sendMail = $("#sendSub");
+
+  function subscribe() {
+
+    var to = $("#subscribe").val()
+    if (to === "") {
+      $('#sendSub').notify(
+        "Please, introduce your email", {
+          elementPosition: 'bottomleft'
+        }
+      );
+    } else {
+      axios.post(`/subscribe`, {
+          to: to,
+          method: "post",
+          withCredentials: true
+        })
+        .then(() => {
+          $('#sendSub').notify(
+            "You are subscribed !!!", "success", {
+              elementPosition: 'topleft'
+            }
+          );
+        })
+        .catch(error => console.error(error));
+    }
+
+  }
+  sendMail.click(subscribe)
+})
